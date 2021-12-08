2fe7cf26c3f482e5fb4cfa9ea4f806a7036fd655
gabriel-ferreira-da-silva <gfs4@cin.ufpe.br>
2021-12-07 22:12:56 -0300

    questao 8 comentada e pronta

--- questao8/questao8.c
+++ questao8/questao8.c
@@ -5,16 +5,37 @@
 #include <string.h>
 #include <math.h>
 
+
+/*
+
+8. Um exemplo muito comum de controle de processo é o Problema da Montanha Russa.
+Nesse problema, existem 3 tipos de threads:
+○ a montanha russa;
+○ os passageiros;
+○ o(s) carrinho(s).
+Para o nosso problema, vamos supor que temos 20 passageiros ao todo e 1 carrinho.
+Os passageiros precisam esperar na fila até que consigam o carrinho para andar na montanha
+russa. O carrinho suporta no máximo 10 passageiros por viagem, e ele só sai da estação
+quando estiver cheio. Depois do passeio, os passageiros, um pouco enjoados, passeiam pelo
+parque antes de retornarem à fila para andar novamente na montanha russa. Porém, por
+motivos de segurança, o carrinho só pode fazer 10 voltas por dia.
+Supondo que o carro e cada passageiro sejam representados por uma thread diferente,
+escreva um programa, usando pthreads, que simule o sistema descrito. Após as 10 voltas, o
+programa deve ser encerrado
+
+*/
+
 #define CARRO_THREADS_NUMBER 1
 #define PASSAGEIROS_THREADS_NUMBER 20
 
 #define MAX_VOLTAS 10
 #define MAX_PASSAGEIROS 10
 
-#define AGUARDANDO 0
+#define AGUARDANDO 0    // ESSES SÃO OS TRES ESTADOS DO CARRO
 #define CORRENDO 1
+#define DESEMBARCANDO 2
 
-#define DESCANSANDO 0
+#define DESCANSANDO 0   // ESSES SÃO OS TRES ESTADOS DOS PASSAGEIROS
 #define NA_FILA 1
 #define NO_CARRO 2
 
@@ -24,63 +45,60 @@ pthread_mutex_t passageiro_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 
-typedef struct{
+typedef struct{      // ESSA ESTRUTURA DEFINE O ALGUMAS CARACTERISTICAS DOS PASSAGEIROS
 	int id;
-	int status;
+	int status;       // O STATUS É O ESTADO DO ATUAL DO PASSAGEIRO
 	int lugar_na_fila;
 	int n_voltas;
 }Passageiro; 
 
-int voltas=0;
 
-int carro_lugar[MAX_PASSAGEIROS];
+// ALGUMAS VARIAVEIS
+int voltas=0;  // VOLTAS QUE O CARRO DÁ
 int estado_carro[CARRO_THREADS_NUMBER];
-int lugar_vazio=0;//[CARRO_THREADS_NUMBER];
-int ultimo_na_fila=0;
+int ultimo_na_fila=0;   //ESSA VARIAVEL CONTROLA O TAMANHO DA FILA
 
 Passageiro passageiro[PASSAGEIROS_THREADS_NUMBER];
 
-void *vida_passageiros(void *tid){
+void *vida_passageiros(void *tid){  // ESSA FUNÇÃO INICIALIZA E ADMINISTRA O ESTADO DO PASSAGEIRO
 	int threadId =  (*(int *)tid);
 
 	srand((long int) &threadId);
 
-	pthread_mutex_lock(&passageiro_mutex);
-	passageiro[threadId/* - CARRO_THREADS_NUMBER*/].id = threadId;
-	passageiro[threadId /*- CARRO_THREADS_NUMBER*/].status = rand()%2;
-	passageiro[threadId /*- CARRO_THREADS_NUMBER*/].lugar_na_fila  = -1;//ultimo_na_fila;	
+	pthread_mutex_lock(&passageiro_mutex);          // AQUI É DEFINIDO O ESTADO INICIAL DO PASSAGEIRO   
+	passageiro[threadId].id = threadId;  
+	passageiro[threadId].status = rand()%2;    		// O ESTADO É DEFINIDO PELA FUNÇÃO RAND()
+	passageiro[threadId].lugar_na_fila  = -1;
 	pthread_mutex_unlock(&passageiro_mutex);
 
-	if(passageiro[threadId].status == NA_FILA){
+	if(passageiro[threadId].status == NA_FILA){    		   // SE ESTADO INICIAL FOR NA_FILA ...
 		pthread_mutex_lock(&carro_mutex); 
-		passageiro[threadId].lugar_na_fila=ultimo_na_fila;
-		ultimo_na_fila++;
+		passageiro[threadId].lugar_na_fila=ultimo_na_fila; // ... ELE É COLOCADO NO ULTIMO LUGAR DA FILA
+		ultimo_na_fila++;								   // AO FINAL O ULTIMO LUGAR É INCREMENTADO
 		pthread_mutex_unlock(&carro_mutex);
 	}
+
 	int this = threadId ;
 	int i,j,k;
 	
-	//***********************************************************************************************************************************
-	//printf("criei passageiro %d\nstatus: %d\nlugar na fila: %d\n\n", threadId, passageiro[this].status, passageiro[this].lugar_na_fila );
 	while(voltas < MAX_VOLTAS){
 
-		if(passageiro[this].status==DESCANSANDO){
+		if(passageiro[this].status==DESCANSANDO){   // APENAS UM ESTADO É DESCRITO POIS OS OUTROS SÃO
+													// ADMINISTRADOS NA FUNÇÃO "corrida()"
 			
-			// UM ROTINA DE ESPERA ONDE O PASSAGEIRO "PASSEIA"
-
+			// PRIMEIRA ELE  ESPERA NESSA ROTINA DE TEMPO ONDE O PASSAGEIRO "PASSEIA"
 			int espera;
 			srand((long int )&espera);
-			espera = (MAX_VOLTAS*(rand()%100)*passageiro[this].n_voltas);
-
-			for(i=0;i < 110 + espera ;i++){	
-				
-				for(j=0;j < 10 + espera  ; j++){
+			espera = rand()%300;
+			for(i=0;i < 250 + espera ;i++){	
+				for(j=0;j < 100 + espera  ; j++){
 				}
 			}
 
+			//DEPOIS ELE ENTRA NA FILA
 			pthread_mutex_lock(&passageiro_mutex);
 			pthread_mutex_lock(&carro_mutex);
-			passageiro[this].status=NA_FILA;
+			passageiro[this].status=NA_FILA;                 
 			passageiro[this].lugar_na_fila = ultimo_na_fila;  
 			ultimo_na_fila++;
 			pthread_mutex_unlock(&passageiro_mutex); 
@@ -92,7 +110,7 @@ void *vida_passageiros(void *tid){
 
 }
 
-void *corrida(void *tid){
+void *corrida(void *tid){  // ESSA FUNÇÃO ADMINISTRA OS TRES ESTADOS DO CARRINHO ATE ATINGIR O LIMITE DE RODADAS
 	int i,j,k;
 	int threadId =  (*(int *)tid);
 
@@ -100,79 +118,73 @@ void *corrida(void *tid){
 
 	while( voltas < MAX_VOLTAS ){
 	
-		if(estado_carro[threadId] == AGUARDANDO){
-			pthread_mutex_lock(&passageiro_mutex); 
-			
-			for( i=0;i < PASSAGEIROS_THREADS_NUMBER && estado_carro[threadId] != CORRENDO ;i++ ){    // checa se alguem esta na fila
-				pthread_mutex_lock(&carro_mutex);
-				if (passageiro[i].status == NA_FILA  && passageiro[i].lugar_na_fila <= MAX_PASSAGEIROS   && passageiro[i].lugar_na_fila >= 0){  // checa se o passageiro esta na fila
-					
-					passageiro[i].status = NO_CARRO;
-					printf("- o passageiro %2d entrou no lugar ocupado %2d\n", passageiro[i].id, lugar_vazio);
-					passageiro[i].n_voltas++;
-					passageiro[i].lugar_na_fila = -1;
-					ultimo_na_fila--;    /// diminui o tamanho da fila
-					carro_lugar[lugar_vazio] =passageiro[i].id; 
-					lugar_vazio++;
-
-					for(j=0; j < PASSAGEIROS_THREADS_NUMBER ;j++){
-						if(passageiro[j].status==NA_FILA){
-							--passageiro[j].lugar_na_fila;	
-						}
-					}
-
-					if(lugar_vazio >= MAX_PASSAGEIROS){
-						estado_carro[threadId]=CORRENDO;
-					} 		
-				}
-				pthread_mutex_unlock(&carro_mutex);
-			}
-			pthread_mutex_unlock(&passageiro_mutex); 
-		}//else{
+		if(estado_carro[threadId] == AGUARDANDO){   // NESSE PRIMEIRO ESTADO ELE APENAS CONFERE SE
+			pthread_mutex_lock(&carro_mutex);   	// HÁ PESSOAS O SUFICIENTE NA FILA
+			if(ultimo_na_fila >= MAX_PASSAGEIROS ){ estado_carro[threadId] = CORRENDO; }  //SE SIM ELE PULA PRO PROXIMO
+			pthread_mutex_unlock(&carro_mutex);
+		}
 	
 		if(estado_carro[threadId] == CORRENDO){
 
-				printf("\n***********************************************************************************\n");
-				printf("o carro esta correndo a %2d° volta\n\n", voltas);
-				for(i=0;i < MAX_PASSAGEIROS;i++){
-					printf("lugar - %2d : passageiro - %2d \n" ,i, carro_lugar[i]);
+			// OS PRIMEIROS PASSAGEIROS ENTRAO NO CARRO
+			pthread_mutex_lock(&passageiro_mutex);
+			for(i=0; i < PASSAGEIROS_THREADS_NUMBER ;i++){ // O TESTE DA LINHA ABAIXO CONFERE QUEM SÃO OS PRIMEIROS PASSAGEIROS DA FILA
+				if(passageiro[i].lugar_na_fila < MAX_PASSAGEIROS && passageiro[i].status == NA_FILA ){
+					passageiro[i].status = NO_CARRO;      // O STATUS DE NA_FILA É TROCADO PARA NO_CARRO
 				}
-		
-				for(i=0;i < 100;i++){
-					for(j=0;j < 100 ;j++){
-					}
-				}
-				
-				int cansados=0;
-				int esperando=0;
+			}
+			pthread_mutex_unlock(&passageiro_mutex);
 
-				
-				for(i=0;i < PASSAGEIROS_THREADS_NUMBER ;i++){
-					pthread_mutex_lock(&passageiro_mutex);
-					if(passageiro[i].status==NO_CARRO){
-						passageiro[i].lugar_na_fila=-1;    //muda lugar na fial
-						passageiro[i].status= DESCANSANDO; // muda status para q ele descanse
+			// ABAIXO É PRINTADO O MENO PRINCIPAL
+
+			printf("\n*************************************************************\n");
+			printf("            o carro esta correndo a %2d° volta\n\n", voltas);
+			printf("     passageiro |              status | lugar na fila/carro |\n");
+			printf("                |                     |                     |\n");
+				for(i=0;i < PASSAGEIROS_THREADS_NUMBER;i++){
+					printf("             %2d |",passageiro[i].id);
+					switch(passageiro[i].status){
+						case NA_FILA:
+							printf("         %11s |                  %2d |\n","NA_FILA", passageiro[i].lugar_na_fila);
+						break;
+						case NO_CARRO:
+							printf("         %11s |                  %2d |\n","NO_CARRO", passageiro[i].lugar_na_fila);
+						break;
+						case DESCANSANDO:
+							printf("         %11s |              NENHUM | \n","DESCANSANDO");
+						break;
+	
 					}
-					pthread_mutex_unlock(&passageiro_mutex);
-					if(passageiro[i].status==DESCANSANDO){
-						cansados++;
-					}
-					if(passageiro[i].status==NA_FILA){
-						esperando++;
-					}
-					
 				}
-				
-				
-				printf("\nnº de passageiros cansados %2d\n", cansados);
-				printf("nº de passageiros na fila %2d\n\n" ,esperando);
-				printf("\n**********************************************************************\n\n");
-				
-				pthread_mutex_lock(&carro_mutex);
-				estado_carro[threadId]=AGUARDANDO;
-				lugar_vazio=0;
-				voltas++;
-				pthread_mutex_unlock(&carro_mutex);
+
+			// PEQUENA ROTINA DE TEMPO SIMULANDO O ANDAR DO CARRO
+			for(i=0;i < 100;i++){
+				for(j=0;j < 100 ;j++){
+				}
+			}
+			// PULA PARA O PROXIMO ESTADO
+			estado_carro[threadId] = DESEMBARCANDO;
+
+		}
+
+		if(estado_carro[threadId] == DESEMBARCANDO){  // ESSE É O ULTIMO ESTADO DO CICLO
+			
+			pthread_mutex_lock(&passageiro_mutex);
+			pthread_mutex_lock(&carro_mutex);
+			ultimo_na_fila-=MAX_PASSAGEIROS; 					//PASSAGEIROS DESEMBARCAM
+
+			for(i=0; i < PASSAGEIROS_THREADS_NUMBER ;i++){
+				passageiro[i].lugar_na_fila-=MAX_PASSAGEIROS;   // A FILA "ANDA"		
+				if(passageiro[i].status == NO_CARRO){           // OS PASSAGEIROS QUE ESTAVAM NO CARRO
+					passageiro[i].status = DESCANSANDO;			// DESCANSAM
+					passageiro[i].n_voltas++;					// ACRESCENTAM O NUMERO DE VOLTAS QUE DERAM
+					passageiro[i].lugar_na_fila=-1;				// E SAEM DA FILA
+				}
+			}
+			pthread_mutex_unlock(&passageiro_mutex);
+			pthread_mutex_unlock(&carro_mutex);
+			voltas++;	//VOLTAS SÃO INCREMENTADAS
+			estado_carro[threadId] = AGUARDANDO; // VOLTA PARA O INICIO
 		}
 	}
 }
@@ -219,6 +231,7 @@ int main (){
 	return 0;	
 
 }
+
 
 
 
